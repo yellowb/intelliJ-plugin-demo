43fcd47e925318e17efcb8d1f4cb705f448cc52e
yellowb <122517167@qq.com>
2019-01-09 10:02:46 +0800

    edit...

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getData(PlatformDataKeys.PROJECT);
-        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        String txt= Messages.showInputDialog(project, project.getName()+"|What is your name?", "Input your name", Messages.getQuestionIcon());
         System.out.println("User input: " + txt);
         Messages.showMessageDialog(project, "HI, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
     }
