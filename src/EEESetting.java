96884e79dd225a844d363da6298079f27b3b68bf
yellowb <122517167@qq.com>
2019-01-16 16:49:51 +0800

    edit...

---
+++ src/EEESetting.java
@@ -0,0 +1,10 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class EEESetting extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        System.out.println("xxx");
+    }
+}
