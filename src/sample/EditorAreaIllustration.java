c3bd6decee04541dac7e608ca191d0de34d4728f
yellowb <122517167@qq.com>
2019-01-24 11:00:52 +0800

    edit...

--- src/sample/EditorAreaIllustration.java
+++ src/sample/EditorAreaIllustration.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -9,19 +10,27 @@ import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.editor.VisualPosition;
 import com.intellij.openapi.ui.Messages;
 
+import static com.intellij.codeInsight.hint.HintManager.HIDE_BY_CARET_MOVE;
+import static com.intellij.codeInsight.hint.HintManager.UNDER;
+
 public class EditorAreaIllustration extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
-        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
         CaretModel caretModel = editor.getCaretModel();
 
         LogicalPosition logicalPosition = caretModel.getLogicalPosition();
         VisualPosition visualPosition = caretModel.getVisualPosition();
         int offset = caretModel.getOffset();
 
-        Messages.showInfoMessage(logicalPosition.toString() + "\n" + visualPosition.toString() + "\n" +
-                "Offset: " + offset, "Caret Info");
+        String message = logicalPosition.toString() + "\n" + visualPosition.toString() + "\n" +
+                "Offset: " + offset;
 
+//        Messages.showInfoMessage(message, "Caret Info");
 
+        System.out.println(message);
+
+//        HintManager.getInstance().showErrorHint(editor, "XXXXX", UNDER);
+        HintManager.getInstance().showInformationHint(editor, "Tom here...");
     }
 }
