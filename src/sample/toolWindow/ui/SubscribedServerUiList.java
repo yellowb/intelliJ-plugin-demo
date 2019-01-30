f44d2b4781b5037470282a454a3f35f6f1de0df4
yellowb <122517167@qq.com>
2019-01-30 14:59:11 +0800

    Implement tool window UI

--- src/sample/toolWindow/ui/SubscribedServerUiList.java
+++ src/sample/toolWindow/ui/SubscribedServerUiList.java
@@ -1,13 +1,16 @@
 package sample.toolWindow.ui;
 
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBList;
 import com.intellij.util.ui.JBUI;
 import sample.toolWindow.SubscribedServerItem;
 import sample.toolWindow.SubscribedServerList;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.EventListener;
@@ -22,6 +25,7 @@ public class SubscribedServerUiList extends JBList<SubscribedServerItem> impleme
         //TODO
         this.setModel(this.model);
         this.setCellRenderer(new SubscribedServerUiCellRenderer());
+        this.addMouseListener(new SubscribedServerUiCellClickListener(this));
         this.syncModel();
     }
 
@@ -52,12 +56,66 @@ public class SubscribedServerUiList extends JBList<SubscribedServerItem> impleme
         @Override
         public Component getListCellRendererComponent(JList<? extends SubscribedServerItem> list, SubscribedServerItem value, int index, boolean isSelected, boolean cellHasFocus) {
             final Color foreground = list.getForeground();
-            final String message = "<html>" + value.getServerAddress() + ":" + value.getServerPort() + "</html>";
-            this.setText(message);
+            final Color background = list.getBackground();
+            String message = "";
             this.setForeground(foreground);
             this.setBorder(JBUI.Borders.empty(2, 10));
 
+            if (isSelected) {
+                message = "<html> # " + value.getServerAddress() + ":" + value.getServerPort() + "</html>";
+                setBackground(JBColor.ORANGE);
+            } else {
+                message = "<html>" + value.getServerAddress() + ":" + value.getServerPort() + "</html>";
+
+                setBackground(background);
+            }
+            this.setText(message);
+
             return this;
+        }
+    }
+
+    static class SubscribedServerUiCellClickListener implements MouseInputListener {
+
+        private final SubscribedServerUiList subscribedServerUiList;
+
+        public SubscribedServerUiCellClickListener(SubscribedServerUiList subscribedServerUiList) {
+            this.subscribedServerUiList = subscribedServerUiList;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.out.println(subscribedServerUiList.getSelectedValue());
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+
         }
     }
 }
